5d67c0ded8e8a7fffae409a59590e9178309f210
Yash Handa <yashhanda500@gmail.com>
2021-09-07 23:31:27 +0530

    lec-12

--- index4.js
+++ index4.js
@@ -2,16 +2,48 @@
 const fs=require('fs');
 const http=require('http');
 
+let changes=(val,tempcard)=>{
+   let output=tempcard.replace("{%image%}",val.image);    
+    output=output.replace("{%productname%}",val.productName);
+    output= output.replace("{%quantity%}",val.quantity);
+    output=output.replace("{%price%}",val.price);
+    output=output.replace("{%id%}",val.id);
+  
+    if(val.organic)
+    output= output.replace("{%not_organic%}","Organic");
+    else
+    output=output.replace("{%not_organic%}","not-organic");
+    return output;
+}
+
 let server=http.createServer((req,res)=>{
     let pathname=req.url;
     let data_json=fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+    let data_object=JSON.parse(data_json);
 
+    let tempoverview=fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
+    let tempcard=fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
+    let tempproduct=fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
+    
+    //overview page
     if(pathname=='/overview' || pathname=='/'){
-        res.end("this is running");
+        res.writeHead(200,'ok',{
+            'Content-type':'text/html',
+        });
+        let cardshtml=data_object.map((val)=>{          //CPU of program.
+            return changes(val,tempcard);
+        })                             //join to conver into string;
+        console.log(cardshtml);
+        cardshtml=cardshtml.join('');   
+
+        const output=tempoverview.replace("{%product-cards%}",cardshtml);
+        res.end(output);
     }
-    else if(pathname=='/over'){
+    //product page
+    else if(pathname=='/product'){
         res.end("we are still working");
     }
+    //API
     else if(pathname=='/api'){
         //now instead of a callback, we are intitially getting data, then using it everytime.
         // fs.readFile(`${__dirname}/dev-data/data.json`,'utf-8',(err,data)=>{
@@ -25,6 +57,7 @@ let server=http.createServer((req,res)=>{
         console.log(JSON.parse(data_json));
         res.end(data_json);
     }
+    //not found
     else{
       res.writeHead(404,"workiiiiiing",{
           "Content-type":"text",
