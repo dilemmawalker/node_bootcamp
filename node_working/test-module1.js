1ddf7f1e7831e17b6e5dfb947bde619d50ace212
Yash Handa <yashhanda500@gmail.com>
2021-09-28 21:15:12 +0530

    exporting classes & using them

--- node_working/test-module1.js
+++ node_working/test-module1.js
@@ -6,7 +6,7 @@ class calc{
         return a*b;
     }
 }
-module.exports=calc;
+module.exports=calc;    //used when we have to export single "thing"
 
 //also we can write as:
 // module.exports=class{
