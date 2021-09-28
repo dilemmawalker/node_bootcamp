6b2d5d11f8ec943c00c02eb48a86d0028d6d50c2
Yash Handa <yashhanda500@gmail.com>
2021-09-28 21:10:47 +0530

    module working

--- node_working/modules.js
+++ node_working/modules.js
@@ -1,3 +1,8 @@
 // console.log(arguments);
 
-console.log(require("module").wrapper);
+// console.log(require("module").wrapper);
+
+const c=require("./test-module1.js");
+const obj=new c();
+console.log(obj.add(2,3));
+console.log(obj.multiply(2,4));
---
+++ node_working/test-module1.js
@@ -0,0 +1,19 @@
+class calc{
+    add(a,b){
+        return a+b;
+    }
+    multiply(a,b){
+        return a*b;
+    }
+}
+module.exports=calc;
+
+//also we can write as:
+// module.exports=class{
+//     add(a,b){
+//         return a+b;
+//     }
+//     multiply(a,b){
+//         return a*b;
+//     }
+// }
