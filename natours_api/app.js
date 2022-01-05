a3f1cfa3bc66b23969cc9ec1e50f2f1242105084
Yash Handa <yashhanda500@gmail.com>
2022-01-05 15:55:52 +0530

    Updated post request

--- natours_api/app.js
+++ natours_api/app.js
@@ -1,13 +1,44 @@
 const express = require('express');
+const fs=require('fs');
 
 app = express();
 
-app.get('/',(req,res)=>{
-    res.send("server says hello");
-})
+app.use(express.json());        //middleware
 
-app.post('/',(req,res)=>{
-    res.status(200).json({message:'Hello World' , text: 'meri marzi'});
+// app.get('/',(req,res)=>{
+//     res.send("server says hello");
+// })
+
+// app.post('/',(req,res)=>{
+//     res.status(200).json({message:'Hello World' , text: 'meri marzi'});
+// })
+
+// app.get('/api/v1/tours',(req,res)=>{
+//         const file=JSON.parse(fs.readFile(`${__dirname}/starter/dev-data/data/tours-simple.json`,(err,data)=>{
+//             // if(err)
+//             // console.log("error");
+//             // else
+//             // console.log(data);    
+//             // return data;                                  
+//         }));
+//     console.log(file);
+//     res.send(file);
+// })
+
+app.get('/api/v1/tours',(req,res)=>{
+    const file = fs.readFile(`${__dirname}/starter/dev-data/data/tours-simple.json`,(err,data)=>{
+        let tour = JSON.parse(data);
+        // console.log(JSON.parse(data));
+        // res.send(JSON.parse(data));
+        res.json({status:"success",
+                  length: tour.length,
+                  data: JSON.parse(data)});
+    });
+});
+
+app.post('/api/vq/tours',(req,res)=>{
+    console.log(req.body);
+    res.send("done");
 })
 
 const port=3000;
