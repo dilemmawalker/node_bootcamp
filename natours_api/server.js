b744fd6a3df64d5c546db7ea417d694b7f08740d
Yash Handa <yashhanda500@gmail.com>
2022-02-08 21:47:14 +0530

    database connection established

--- natours_api/server.js
+++ natours_api/server.js
@@ -1,8 +1,24 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: `./config.env` });
-console.log(process.env);
+// console.log(process.env);
+
+const DB=process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB,{useNewUrlParser:true,
+useCreateIndex:true,
+useFindAndModify:false,
+useUnifiedTopology: true })
+.then(con=>{
+  // console.log(con.connections);
+  console.log("running");
+})
+// mongoose.connect(DB).then((con)=>{
+//   console.log(con.connections);
+//   console.log("database connection established");
+// })
 
 //start server
 console.log(process.env.PORT);
