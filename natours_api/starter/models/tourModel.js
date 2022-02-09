8ca6baf95c37ac8d46ac52338557d390791ac7bd
Yash Handa <yashhanda500@gmail.com>
2022-02-09 20:37:37 +0530

    Tour Schema Changes

--- natours_api/starter/models/tourModel.js
+++ natours_api/starter/models/tourModel.js
@@ -1,8 +1,53 @@
 const mongoose = require('mongoose');
 const tourSchema=new mongoose.Schema({
-    name:String,
-    rating:Number,
-    price:Number
+    name:{
+      type:String,
+      trim:true,
+    },
+    price:Number,
+    maxGroupSize:{
+      type:Number,
+      required:[true,'Group size is necessary']
+    },
+    duration:{
+      type:Number,
+      required:true
+    },
+    difficulty:{
+      type:String,
+      required:true
+    },
+    ratingsAverage:{
+      type:Number,
+      default:0
+    },
+    ratingsQuantity:{
+      type:Number,
+      default:0
+    },
+    priceDiscount:{
+      type:Number
+    },
+    summary:{
+      type:String,
+      trim:true,
+      required:true
+    },
+    description:{
+      type:String,
+      trim:true,
+    },
+    imageCover:{
+      type:String,
+      required:true,
+    },
+    images:[String],
+    createdAt:{
+      type:Date,
+      default:Date.now()
+    },
+    startDates:[Date],
+
   });
   const Tour = mongoose.model('Tour',tourSchema);
 
